2493289a7fc33813a15ef0d8da8765c62fe4838b
VRT36 <45532566+VRT36@users.noreply.github.com>
2021-03-02 21:02:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function vrt(){
     'May your birthday and your life be as wonderful as you are . Happy Birthday',
     'May you achieve everything you desire in life. I wish you a very sweet and happy birthday. May you have an awesome life ahead. Enjoy your day',
     'You are very special and you deserve the best. I wish you a wonderful life filled with love and happiness. Happy Birthday.',
-    'Happy Birthday you are a beautiful person, inside and out .I hope that you remember that,today and always.',
+    'Happy Birthday, you are a beautiful person, inside and out .I hope that you remember that,today and always.',
     'Forget the fact that you are aging,and let us start celebrating!Happy birthday!',
     'Some special words on your birthday: keep smiling as long as you’ve those teeth.Happy Birthday',
     'It’s always good to have a birthday, but to me it’s another chance to eat lots more cakes. Happy birthday'
