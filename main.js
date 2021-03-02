2aa76212a4b1507c481da2504a27e0927b13b86d
VRT36 <45532566+VRT36@users.noreply.github.com>
2021-03-02 19:13:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+function vrt(){
+  document.getElementById("name").innerHTML = document.location.href.replace('https://bday.vrt36.com/?vogel=','');
+  var bdayArray = [
+    'On your birthday I wish you success and endless happiness!.Wishing you an awesome birthday!',
+    'Wish you a beautiful birthday and I hope you get double of everything you want in your life. Happy Birthday!'
+  ];
+  var arrNum = Math.floor(Math.random()*bdayArray.length)
+  document.getElementById("bday-text").innerHTML = bdayArray[arrNum]);
+}
